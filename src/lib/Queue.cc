771aaa642565281326d3cfacc82e1f2d41194121
merschie <f-mersch@web.de>
2023-05-22 09:48:14 +0200

    implemented queue

---
+++ src/lib/Queue.cc
@@ -0,0 +1,20 @@
+#include "Queue.h"
+
+
+void Queue::addElement(void* data) {
+    QueueElement* newElement = new QueueElement;
+    newElement->data = data;
+    newElement->next = 0;
+    while (first->next != 0) {
+        first = first->next;
+    }
+    first->next = newElement;
+}
+
+void* Queue::getFirst() {
+    QueueElement* tmp = first;
+    first = first->next;
+    void* data = tmp->data;
+    delete tmp;
+    return data;
+}
---
+++ src/lib/Queue.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ *                                                                           *
+ *                              Q U E U E                                    *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * Beschreibung:    Implementierung einer einfach verketteten Liste von      *
+ *                  QUEUE Objekten.                                          *
+ *                                                                           *
+ * Autor:           Fabian Mersch                                            *
+ *****************************************************************************/
+
+
+class Queue {
+
+struct QueueElement
+{
+    QueueElement* next;
+    void* data;
+};
+    
+private:
+    Queue(const Queue &copy); // Verhindere Kopieren
+    QueueElement* first;
+
+public:
+    void* getFirst();
+    void addElement(void* data);
+    Queue() {}
+};
+
